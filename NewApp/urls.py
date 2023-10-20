e8bd9347fd7e5a8950379b5f6d27b47fe9d2ae1c
SusyP <susanapagliari@gmail.com>
2023-10-20 13:33:43 -0300

    20.10.13.32

--- NewApp/urls.py
+++ NewApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from NewApp import views
 
+
 urlpatterns = [
 path("crear padrino/", views.PadrinoCreateView.as_view() ,name = "crear padrino"), 
 path("padrino/<int:pk>/", views.PadrinoDetailView.as_view() ,name ="detail padrino"),
 path("padrino/", views.PadrinoListView.as_view() ,name ="listar padrino"),
-path("padrino/<int:pk>/editar", views.PadrinoUpdateView.as_view() ,name ="update padrino"),
+path("padrino/<int:pk>/editar", views.PadrinoUpdateView.as_view() ,name ="update padrino"),
 ]
