7a7836c1c84bd5fe207ea1d6fe10f0fc16e8bb2b
SusyP <susanapagliari@gmail.com>
2023-11-05 11:50:31 -0300

    5111150

--- DogsApp/models.py
+++ DogsApp/models.py
@@ -29,6 +29,7 @@ class Refugio(models.Model):
 class Avatar(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
+    descripcion = models.TextField(null=True,blank=True)
     def __str__(self):
         return f"{self.user} {self.imagen}"
     
--- DogsApp/views.py
+++ DogsApp/views.py
@@ -154,26 +154,45 @@ def registroview(request):
     return render(request,"temp_app/registro.html", {"form":form})
   
 # @permission_required('DogsApp.change_avatar')
+
 def editarview(request):
-    
-    
-    
     usuario=request.user
-    if request.method == "POST":	
-       form=UserEditForm(request.POST,request.FILES,instance=usuario)
-       if form.is_valid():
-        if usuario.has_perm('DogsApp.change_avatar'):
-            avatar_anterior = Avatar.objects.filter(user=request.user)
-            if (len(avatar_anterior) > 0):
-             avatar_anterior.delete()
-            avatar_nuevo = Avatar(user=request.user, imagen=form.cleaned_data["avatar"])
-            avatar_nuevo.save()
-        form.save()
-        return redirect("inicio")
+    if request.method == "POST":
+        form=UserEditForm(request.POST,request.FILES,instance=usuario)
+        if form.is_valid():
+            if usuario.has_perm('DogsApp.change_avatar'):
+                if 'avatar' in request.FILES:  # Verificar si se ha cargado un nuevo avatar
+                    avatar_anterior = Avatar.objects.filter(user=request.user)
+                    if (len(avatar_anterior) > 0):
+                        avatar_anterior.delete()
+                    avatar_nuevo = Avatar(user=request.user, imagen=form.cleaned_data["avatar"], texto=form.cleaned_data["texto"])
+                    avatar_nuevo.save()
+            form.save()
+            return redirect("inicio")
     else:
         form = UserEditForm(instance=usuario)
 
-    return render(request, "temp_app/editar_perfil.html", {"form": form})
+    return render(request, "temp_app/editar_perfil.html",{"form":form})
+# def editarview(request):
+    
+    
+    
+#     usuario=request.user
+#     if request.method == "POST":	
+#        form=UserEditForm(request.POST,request.FILES,instance=usuario)
+#        if form.is_valid():
+#         if usuario.has_perm('DogsApp.change_avatar'):
+#             avatar_anterior = Avatar.objects.filter(user=request.user)
+#             if (len(avatar_anterior) > 0):
+#              avatar_anterior.delete()
+#             avatar_nuevo = Avatar(user=request.user, imagen=form.cleaned_data["avatar"])
+#             avatar_nuevo.save()
+#         form.save()
+#         return redirect("inicio")
+#     else:
+#         form = UserEditForm(instance=usuario)
+
+#     return render(request, "temp_app/editar_perfil.html", {"form": form})
 
      
            
